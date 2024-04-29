c31a5a4b2ce652edad2f6da8528bfb814f6f2a49
Andy Gayton <andy@thecablelounge.com>
2024-04-29 03:28:14 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use std::sync::mpsc;
+use signal_hook::iterator::Signals;
+use std::io::Error;
 
-fn main() {
-    let (tx, rx) = mpsc::channel();
+fn handle_signals() -> Result<(), Error> {
+    let mut signals = Signals::new(&[
+        signal_hook::consts::SIGTERM,
+        signal_hook::consts::SIGINT,
+        signal_hook::consts::SIGPIPE,
+        signal_hook::consts::SIGHUP,
+    ])?;
 
-    ctrlc::set_handler(move || {
-        let _ = tx.send(());
-    })
-    .expect("Error setting Ctrl-C handler");
-
-    eprintln!("Waiting for Ctrl-C...");
-    match rx.recv() {
-        Ok(()) => eprintln!("CTRL-C received."),
-        Err(e) => eprintln!("Error: {}", e),
+    eprintln!("waiting for signals...");
+    for signal in signals.forever() {
+        eprintln!(
+            "received {}",
+            signal_hook::low_level::signal_name(signal).unwrap_or(&format!("unknown: {}", signal))
+        );
+        match signal {
+            signal_hook::consts::SIGTERM | signal_hook::consts::SIGINT => {
+                break;
+            }
+            signal_hook::consts::SIGHUP => (),
+            _ => unreachable!(),
+        }
     }
 
-    println!("Exiting...");
+    Ok(())
+}
+
+fn main() {
+    eprintln!("ctrlc pid: {}", std::process::id());
+    if let Err(e) = handle_signals() {
+        eprintln!("error handling signals: {}", e);
+    }
 }
