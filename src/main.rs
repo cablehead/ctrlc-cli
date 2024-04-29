754bab759453c417183748f85ebd1cc28f2252cc
Andy Gayton <andy@thecablelounge.com>
2024-04-28 20:24:14 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     ctrlc::set_handler(move || {
         let _ = tx.send(());
-    }).expect("Error setting Ctrl-C handler");
+    })
+    .expect("Error setting Ctrl-C handler");
 
-    println!("Waiting for Ctrl-C...");
+    eprintln!("Waiting for Ctrl-C...");
     match rx.recv() {
-        Ok(()) => println!("CTRL-C received."),
+        Ok(()) => eprintln!("CTRL-C received."),
         Err(e) => eprintln!("Error: {}", e),
     }
 
